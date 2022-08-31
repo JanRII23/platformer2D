1c13dfcb6bc129005b18cc69723aa7086cca950b
JRII_Ataka <jrejasii@gmail.com>
2022-08-31 17:26:08 -0500

    finished the jump and moving left and right animation

--- tutorialPlatformer/Assets/Scripts/PlayerMovement.cs
+++ tutorialPlatformer/Assets/Scripts/PlayerMovement.cs
@@ -6,6 +6,12 @@ public class PlayerMovement : MonoBehaviour
 {
 
     private Rigidbody2D rb; //caching the components; make sure to make it private and not expose it to use in other scripts
+    private SpriteRenderer sprite;
+    private Animator anim;
+
+    private float dirX = 0f;
+    [SerializeField] private float moveSpeed = 7f; //serializedfield allows the edits of value in the editor
+    [SerializeField] private float jumpForce = 14f;
     
     //data types int = 16, float = 4.45f, string = "bla", bool = true/false
 
@@ -13,14 +19,16 @@ public class PlayerMovement : MonoBehaviour
     private void Start() //methods should be private as well
     {
         rb = GetComponent<Rigidbody2D>(); //do this you can use the component
+        sprite = GetComponent<SpriteRenderer>();
+        anim = GetComponent<Animator>(); //animator component
     }
 
     // Update is called once per frame
     private void Update()
     {
         //This actually has joystick support
-        float dirX = Input.GetAxisRaw("Horizontal"); //getAxis has slight decelaration, getAxisRaw tries to minimize it
-        rb.velocity = new Vector2(dirX * 7f, rb.velocity.y);
+        dirX = Input.GetAxisRaw("Horizontal"); //getAxis has slight decelaration, getAxisRaw tries to minimize it
+        rb.velocity = new Vector2(dirX * moveSpeed, rb.velocity.y);
 
         //when creating variables make sure to keep in the smallest scope, notice how dirX was not initialize similar to rb up top
 
@@ -30,7 +38,33 @@ public class PlayerMovement : MonoBehaviour
             //getkey has the effect of constantly adding velocity is a key is pressed 
             //getkeyDown only applies for a brief time --> note that both these types do not refer to the input manager in Unity but hard coded
         {
-           rb.velocity = new Vector3(rb.velocity.x, 14f, 0); //vector3(x, y, z), optional but can also use Vector2
+           rb.velocity = new Vector3(rb.velocity.x, jumpForce, 0); //vector3(x, y, z), optional but can also use Vector2
         }
+
+        //note that transition can be paused momentarily
+
+        UpdateAnimationState();
+       
+    }
+
+    private void UpdateAnimationState()
+    {
+        if (dirX > 0f)
+        {
+            //make sure spelling within animator is exact
+            anim.SetBool("running", true); //running right
+            sprite.flipX = false;
+
+        }
+        else if (dirX < 0f) //note that i Know you can change which direction the character is facing via sprite flipX
+        {
+            anim.SetBool("running", true);//running left
+            sprite.flipX = true;
+        }
+        else
+        {
+            anim.SetBool("running", false);
+        }
+
     }
 }
